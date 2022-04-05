80f9aa97e9a0142e775e41990a96bb0a8551fd13
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-04-05 12:01:07 +0300

    Mail işlemleri ile ilgili class eklendi.

---
+++ ECommerceLiteBLL/Settings/SiteSettings.cs
@@ -0,0 +1,112 @@
+﻿using ECommerceLiteEntity.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ECommerceLiteBLL.Settings
+{
+    public class SiteSettings
+    {
+        // To do: Mail adresini webconfig dosyasından çekmeyi de öğrenelim.
+
+        public static string SiteMail { get; set; } = "nayazilim303@gmail.com";
+
+        public static string SiteMailPassword { get; set; } = "betul303303";
+
+        public static string SiteMailSmtpHost { get; set; } = "smtp.gmail.com";
+        public static int SiteMailSmtpPort { get; set; } = 587;
+
+        public static bool SiteMailEnableSSl = true;
+
+        public async static Task SendMail(MailModel model)
+        {
+            try
+            {
+                using (var smtp = new SmtpClient())
+                {
+                    var message = new MailMessage();
+                    message.To.Add(new MailAddress(model.To));
+                    message.From = new MailAddress(SiteMail);
+                    message.Subject = model.Subject;
+                    message.IsBodyHtml = true;
+                    message.Body = model.Message;
+                    message.BodyEncoding = Encoding.UTF8;
+
+                    if (!string.IsNullOrEmpty(model.Cc))
+                    {//modeldeki cc boş değilse.
+
+                        message.CC.Add(new MailAddress(model.Cc));
+
+                    }
+                    if (!string.IsNullOrEmpty(model.Bcc))   //modeldeki bcc boş değilse.
+                    {//modeldeki cc boş değilse.
+
+                        message.Bcc.Add(new MailAddress(model.Bcc));
+
+                    }
+
+                    var networkCredentials = new NetworkCredential()
+                    {
+                        UserName = SiteMail,
+                        Password = SiteMailPassword
+                    };
+
+                    smtp.Credentials = networkCredentials;
+                    smtp.Host = SiteMailSmtpHost;
+                    smtp.Port = SiteMailSmtpPort;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                // To Do: ex loglanacak
+                // loglama: kayıt tutmak.
+            }
+        }
+
+        public static string StringCharacterConverter(string name)
+        {
+            string resultString=
+              name.Replace("'", "")
+            .Replace(" ", "-")
+            .Replace("<", "")
+            .Replace(">", "")
+            .Replace("&", "")
+            .Replace("[", "")
+            .Replace("!", "")
+            .Replace("]", "")
+            .Replace("ı", "i")
+            .Replace("ö", "o")
+            .Replace("ü", "u")
+            .Replace("ş", "s")
+            .Replace("ç", "c")
+            .Replace("ğ", "g")
+            .Replace("İ", "I")
+            .Replace("Ö", "O")
+            .Replace("Ü", "U")
+            .Replace("Ş", "S")
+            .Replace("Ç", "C")
+            .Replace("Ğ", "G")
+            .Replace("|", "")
+            .Replace(".", "-")
+            .Replace("?", "-")
+            .Replace(";", "-");
+
+            return resultString.ToLower();
+        } 
+
+
+
+
+    }
+}
---
+++ ECommerceLiteWEntity/ViewModels/MailModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceLiteEntity.ViewModels
+{
+    public class MailModel
+    {
+        public string To { get; set; }
+
+        public string Cc { get; set; }
+
+        public string Bcc { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+
+
+
+    }
+}
