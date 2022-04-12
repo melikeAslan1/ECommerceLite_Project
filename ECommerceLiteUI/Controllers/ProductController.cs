02878f7f18bd1de92db72da7656721d6a7785452
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-04-12 14:17:02 +0300

    303 logosu eklendi. ProductController eklendi. CRUD işlemlerine başlayacağız.

---
+++ ECommerceLiteUI/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceLiteUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Bu controller a Admin gibi yetkili kişiler erişecektir.
+        //Burada ürünlerin listelenmesi, ekleme, silme, güncelleme işlemleri yapılacaktır.
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+
+
+    }
+}
