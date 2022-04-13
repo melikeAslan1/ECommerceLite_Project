94bad18220720a19bd38d495c9ffe971624d444b
Melike Aslan <1melike2@gmail.com>
2022-04-14 01:39:07 +0300

    Product/Create test ediliyor.

--- ECommerceLiteUI/Controllers/ProductController.cs
+++ ECommerceLiteUI/Controllers/ProductController.cs
@@ -14,8 +14,8 @@ namespace ECommerceLiteUI.Controllers
     public class ProductController : Controller
     {
         CategoryRepo myCategoryRepo = new CategoryRepo();
-        ProductRepo myProductRepo=new ProductRepo();
-        ProductPictureRepo myProductPictureRepo=new ProductPictureRepo();   
+        ProductRepo myProductRepo = new ProductRepo();
+        ProductPictureRepo myProductPictureRepo = new ProductPictureRepo();
 
         //Bu controller a Admin gibi yetkili kişiler erişecektir.
         //Burada ürünlerin listelenmesi, ekleme, silme, güncelleme işlemleri yapılacaktır.
@@ -34,22 +34,21 @@ namespace ECommerceLiteUI.Controllers
             //Linq
             //select * from Categories where BaseCategoryId isnot null
 
-            myCategoryRepo.AsQueryable().Where(x => x.BaseCategoryId != null).ToList().ForEach
-                (x => subCategories.Add(
+            myCategoryRepo.AsQueryable().Where(x => x.BaseCategoryId != null).ToList().ForEach  //?
+                (x => subCategories.Add(new SelectListItem()
+                {
+                    Text = x.CategoryName,
+                    Value = x.Id.ToString()
+                }));
 
-                    new SelectListItem()
-                    {
-                        Text=x.CategoryName,
-                        Value=x.Id.ToString()
-                    }));
 
-                    ViewBag.SubCategories = subCategories;
+            ViewBag.SubCategories = subCategories;
 
             return View();
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken] 
+        [ValidateAntiForgeryToken]
         public ActionResult Create(ProductViewModel model)
         {
             try
@@ -71,13 +70,13 @@ namespace ECommerceLiteUI.Controllers
                 ViewBag.SubCategories = subCategories;
 
 
-                if (!ModelState.IsValid)
+                if (!ModelState.IsValid)  //kullanıcı girişinin doğrulanma olayı.  
                 {
                     ModelState.AddModelError("", "Veri girişleri düzgün olmalıdır");
                     return View(model);
                 }
 
-                if(model.CategoryId<=0 || model.CategoryId> myCategoryRepo.GetAll().Count())
+                if (model.CategoryId <= 0 || model.CategoryId > myCategoryRepo.GetAll().Count())
                 {
                     ModelState.AddModelError("", "Ürüne ait kategori seçilmelidir!");
                     return View(model);
@@ -88,54 +87,54 @@ namespace ECommerceLiteUI.Controllers
 
                 Product product = new Product()
                 {
-                    ProductName=model.ProductName,
-                    Description=model.Description,
-                    ProductCode=model.ProductCode,
-                    CategoryId=model.CategoryId,
-                    Discount=model.Discount,
-                    Quantity=model.Quantity,    
-                    RegisterDate=DateTime.Now,
-                    Price=model.Price
+                    ProductName = model.ProductName,
+                    Description = model.Description,
+                    ProductCode = model.ProductCode,
+                    CategoryId = model.CategoryId,
+                    Discount = model.Discount,
+                    Quantity = model.Quantity,
+                    RegisterDate = DateTime.Now,
+                    Price = model.Price
                 };
 
-                int insertResult=myProductRepo.Insert(product);
-                if(insertResult>0)
+                int insertResult = myProductRepo.Insert(product);
+                if (insertResult > 0)
                 {
                     //Sıfırdan büyükse product tabloya eklendi.
                     //Acaba bu product a resim seçmiş mi? resim seçtiyse o resimlerin yollarını kayıt et.
 
-                    if(model.Files.Any())
+                    if (model.Files.Any())
                     {
 
                         ProductPicture productPicture = new ProductPicture();
                         productPicture.ProductId = product.Id;
                         productPicture.RegisterDate = DateTime.Now;
-                        int counter = 1;  // bizim sistemde resim adedi 5 olarak belirlendiği için.
+                        int counter = 1;  // bizim sistemde resim adedi 5 olarak belirlendiği için.  ilk olarak model.Files a img nin geçişi nerede oluyor ?
 
                         foreach (var item in model.Files)
                         {
                             if (counter == 5) break;
-                            if(item!=null && item.ContentType.Contains("image")&& item.ContentLength>0)
+                            if (item != null && item.ContentType.Contains("image") && item.ContentLength > 0)  //content length ile image in içi boş değil mi demeye calısıyor ?
                             {
                                 string filename = SiteSettings.StringCharacterConverter(model.ProductName).ToLower().Replace("-", "");
 
-                                string extensionName = Path.GetExtension(item.FileName);
+                                string extensionName = Path.GetExtension(item.FileName);   //kaldım.
 
                                 string directoryPath = Server.MapPath($"~/ProductPictures/{filename}/{model.ProductCode}");
 
                                 string guid = Guid.NewGuid().ToString().Replace("-", "");
 
-                                string filePath = Server.MapPath($"~/ProductPictures/{filename}/{model.ProductCode}/") + filename + "-"+ counter + "-" + guid + extensionName;
+                                string filePath = Server.MapPath($"~/ProductPictures/{filename}/{model.ProductCode}/") + filename + "-" + counter + "-" + guid + extensionName;
 
-                                if(!Directory.Exists(directoryPath))
+                                if (!Directory.Exists(directoryPath))
                                 {
                                     Directory.CreateDirectory(directoryPath);
                                 }
                                 item.SaveAs(filePath);
                                 //TODO: Buraya birisi çeki düzen verse süper olur
-                                if(counter==1)
+                                if (counter == 1)
                                 {
-                                    productPicture.ProductPicture1= $"/ProductPictures/{filename}/{model.ProductCode}/" +filename + "-" + counter + "-" + guid + extensionName;
+                                    productPicture.ProductPicture1 = $"/ProductPictures/{filename}/{model.ProductCode}/" + filename + "-" + counter + "-" + guid + extensionName;
                                 }
                                 if (counter == 2)
                                 {
@@ -156,9 +155,9 @@ namespace ECommerceLiteUI.Controllers
 
                         //}
 
-                        int productPictureInsertResult=myProductPictureRepo.Insert(productPicture);
+                        int productPictureInsertResult = myProductPictureRepo.Insert(productPicture);
 
-                        if(productPictureInsertResult>0)
+                        if (productPictureInsertResult > 0)
                         {
                             return RedirectToAction("ProductList", "Product");
                         }
@@ -185,7 +184,7 @@ namespace ECommerceLiteUI.Controllers
                 }
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 ModelState.AddModelError("", "Beklenmedik bir hata oluştu");
                 // ex loglanacak
