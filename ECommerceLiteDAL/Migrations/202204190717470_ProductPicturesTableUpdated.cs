c0803214aa71e13485217d0de1c6db4080f320eb
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-04-19 13:13:00 +0300

    Partial konusu işlendi. Testler yapıldı.

---
+++ ECommerceLiteDAL/Migrations/202204190717470_ProductPicturesTableUpdated.cs
@@ -0,0 +1,42 @@
+﻿namespace ECommerceLiteDAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ProductPicturesTableUpdated : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "IsDeleted", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Orders", "IsDeleted", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Order Details", "IsDeleted", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Products", "IsDeleted", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Categories", "IsDeleted", c => c.Boolean(nullable: false));
+            AddColumn("dbo.ProductPictures", "Picture", c => c.String(maxLength: 400));
+            AddColumn("dbo.ProductPictures", "IsDeleted", c => c.Boolean(nullable: false));
+            AddColumn("dbo.AspNetRoles", "IsDeleted", c => c.Boolean());
+            DropColumn("dbo.ProductPictures", "ProductPicture1");
+            DropColumn("dbo.ProductPictures", "ProductPicture2");
+            DropColumn("dbo.ProductPictures", "ProductPicture3");
+            DropColumn("dbo.ProductPictures", "ProductPicture4");
+            DropColumn("dbo.ProductPictures", "ProductPicture5");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.ProductPictures", "ProductPicture5", c => c.String(maxLength: 400));
+            AddColumn("dbo.ProductPictures", "ProductPicture4", c => c.String(maxLength: 400));
+            AddColumn("dbo.ProductPictures", "ProductPicture3", c => c.String(maxLength: 400));
+            AddColumn("dbo.ProductPictures", "ProductPicture2", c => c.String(maxLength: 400));
+            AddColumn("dbo.ProductPictures", "ProductPicture1", c => c.String(maxLength: 400));
+            DropColumn("dbo.AspNetRoles", "IsDeleted");
+            DropColumn("dbo.ProductPictures", "IsDeleted");
+            DropColumn("dbo.ProductPictures", "Picture");
+            DropColumn("dbo.Categories", "IsDeleted");
+            DropColumn("dbo.Products", "IsDeleted");
+            DropColumn("dbo.Order Details", "IsDeleted");
+            DropColumn("dbo.Orders", "IsDeleted");
+            DropColumn("dbo.AspNetUsers", "IsDeleted");
+        }
+    }
+}
---
+++ ECommerceLiteUI/Controllers/PartialsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ECommerceLiteBLL.Repository;
+
+namespace ECommerceLiteUI.Controllers
+{
+    public class PartialsController : Controller
+    {
+        CategoryRepo myCategoryRepo = new CategoryRepo();
+        ProductRepo myProductRepo = new ProductRepo();
+        public PartialViewResult AdminSideBarResult()
+        {
+            return PartialView("_PartialAdminSideBar");
+        }
+        public PartialViewResult AdminSideBarMenuResult()
+        {
+            TempData["CategoryCount"] = myCategoryRepo.GetAll().Count();
+            return PartialView("_PartialAdminSideBarMenu");
+        }
+
+        public PartialViewResult AdminSideBarProducts()
+        {
+            TempData["ProductCount"] = myProductRepo.GetAll().Count();
+
+            return PartialView("_PartialAdminSideBarProducts");
+        }
+    }
+}
